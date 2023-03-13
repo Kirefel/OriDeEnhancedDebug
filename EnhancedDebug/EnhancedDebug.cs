4b7efd8b4434c9f58d0ae404eecce23c293e584b
Kirefel <kirefelan@gmail.com>
2023-03-13 16:11:13 +1100

    Initial commit

---
+++ EnhancedDebug/EnhancedDebug.cs
@@ -0,0 +1,25 @@
+﻿using BaseModLib;
+using HarmonyLib;
+using OriDeModLoader;
+
+namespace EnhancedDebug
+{
+    public class EnhancedDebug : IMod
+    {
+        public string Name => "Enhanced Debug";
+
+        private Harmony harmony;
+
+        public void Init()
+        {
+            harmony = new Harmony("enhanced_debug");
+            
+            Controllers.Add<EnhancedDebugController>();
+        }
+
+        public void Unload()
+        {
+            harmony.UnpatchAll("enhanced_debug");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# EnhancedDebug
+
+TODO improve developer workflow
+
+## Usage
+
+* Build
+* Copy dll to mod directory
+* Manually add the mod info to manifest.json
+
+## Adding to mod manager
+
+See [the mod index GitHub page](https://github.com/Kirefel/ori-bf-mod-index) to add your mod to the manager
